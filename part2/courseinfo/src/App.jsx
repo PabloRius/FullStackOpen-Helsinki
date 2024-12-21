6055bda9ed28ee61222a65464a7a9d4f82f37ed0
PabloRius <pablogrius@gmail.com>
2024-12-21 12:44:40 +0100

    Fix course key attribute

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -50,7 +50,7 @@ const App = () => {
     <>
       <h1>Web development curriculum</h1>
       {courses.map((course) => {
-        return <Course course={course} />;
+        return <Course key={course.id} course={course} />;
       })}
     </>
   );
