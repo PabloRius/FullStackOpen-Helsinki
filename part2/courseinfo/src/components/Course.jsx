8f5d227e728145d66239bd1a07eefa13d33dbff9
PabloRius <pablogrius@gmail.com>
2024-12-21 12:40:08 +0100

    courseinfo 2.4 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -4,11 +4,11 @@ import { Total } from "./Total";
 
 export const Course = (props) => {
   const { course } = props;
-  const { title, parts } = course;
+  const { name, parts } = course;
   const total = parts.reduce((acc, part) => acc + part.exercises, 0);
   return (
     <div>
-      <Header course={title} />
+      <Header course={name} />
       <Content parts={parts} />
       <Total total={total} />
     </div>
