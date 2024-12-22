7d39381678c29fbf275c57f49311c44eb2fe5cce
PabloRius <pablogrius@gmail.com>
2024-12-22 12:23:40 +0100

    Fix 3.2 getOne by id

--- part3/routes/persons.js
+++ part3/routes/persons.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getAll } from "../services/persons.js";
+import { getAll, getOne } from "../services/persons.js";
 
 export const persons_api = express();
 
@@ -12,4 +12,15 @@ persons_api.get("/", async (req, res) => {
       .send("<p>Error retrieving the data form the server</p>");
   }
   return res.status(200).json(persons);
+});
+
+persons_api.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const person = await getOne(id);
+  if (person.length === 0) {
+    return res.status(404).send(`<p>Element not found on the server</p>`);
+  } else if (!person) {
+    return res.status(500).send(`<p>Internal server error</p>`);
+  }
+  return res.status(200).json(person);
 });
--- part3/services/persons.js
+++ part3/services/persons.js
@@ -10,4 +10,28 @@ const getAll = async () => {
   }
 };
 
-export { getAll };
+const getOne = async (id) => {
+  try {
+    const result = await axios.get(`http://localhost:3002/persons?id=${id}`);
+
+    return result.data;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+const deleteOne = async (id) => {
+  try {
+    const result = await axios.get(`http://localhost:3002/persons/${id}`);
+
+    console.log(result);
+    if (filtered) return filtered[0];
+    else return undefined;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export { getAll, getOne };
