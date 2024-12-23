702d163b39cfec53356963bf781d407cff76c324
PabloRius <pablogrius@gmail.com>
2024-12-23 12:02:03 +0100

    part3 3.6

--- part3/routes/persons.js
+++ part3/routes/persons.js
@@ -1,38 +1,74 @@
 import express from "express";
 
-import { deleteOne, getAll, getOne } from "../services/persons.js";
+import { createOne, deleteOne, getAll, getOne } from "../services/persons.js";
 
 export const persons_api = express();
 
 persons_api.get("/", async (req, res) => {
-  const persons = await getAll();
-  if (!persons) {
+  try {
+    const persons = await getAll();
+
+    return res.status(200).json(persons);
+  } catch (e) {
+    console.error(e);
     return res
-      .status(500)
-      .send("<p>Error retrieving the data form the server</p>");
+      .status(e.status)
+      .json({ error: e.message || "Internal server error" });
   }
-  return res.status(200).json(persons);
 });
 
 persons_api.get("/:id", async (req, res) => {
   const id = req.params.id;
-  const person = await getOne(id);
-  if (person.length === 0) {
-    return res.status(404).send(`<p>Element not found on the server</p>`);
-  } else if (!person) {
-    return res.status(500).send(`<p>Internal server error</p>`);
+  try {
+    const person = await getOne(id);
+    return res.status(200).json(person);
+  } catch (e) {
+    console.error(e);
+    switch (e.status) {
+      case 404:
+        return res
+          .status(404)
+          .json({ error: `ID ${id} doesn't exist on the server` });
+
+      default:
+        return res
+          .status(e.status)
+          .json({ error: e.message || "Internal server error" });
+    }
   }
-  return res.status(200).json(person);
+});
+
+persons_api.post("/", async (req, res) => {
+  try {
+    const { name, number } = req.body;
+    const random_id = Math.floor(Math.random() * 100);
+    const person = await createOne(random_id, name, number);
+    return res.status(200).json(person);
+  } catch (e) {
+    console.error(e);
+    return res
+      .status(e.status)
+      .json({ error: e.message || "Internal server error" });
+  }
 });
 
 persons_api.delete("/:id", async (req, res) => {
   const id = req.params.id;
-  const person = await deleteOne(id);
-  console.log(person);
-  if (Object.keys(person).length === 0) {
-    return res.status(404).send(`<p>Element not found on the server</p>`);
-  } else if (!person) {
-    return res.status(500).send(`<p>Internal server error</p>`);
+  try {
+    const person = await deleteOne(id);
+    return res.status(200).json(person);
+  } catch (e) {
+    console.error(e);
+    switch (e.status) {
+      case 404:
+        return res
+          .status(404)
+          .json({ error: `ID ${id} doesn't exist on the server` });
+
+      default:
+        return res
+          .status(e.status)
+          .json({ error: e.message || "Internal server error" });
+    }
   }
-  return res.status(200).json(person);
 });
--- part3/services/persons.js
+++ part3/services/persons.js
@@ -1,35 +1,51 @@
 import axios from "axios";
+import { StatusError } from "../custom/error.js";
 
 const getAll = async () => {
-  try {
-    const result = await axios.get("http://localhost:3002/persons");
-    return result.data;
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
+  const result = await axios.get("http://localhost:3002/persons");
+  return result.data;
 };
 
 const getOne = async (id) => {
-  try {
-    const result = await axios.get(`http://localhost:3002/persons/${id}`);
+  if (!id) throw new StatusError(400, "An id must be specified");
 
-    return result.data;
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
+  const result = await axios.get(`http://localhost:3002/persons/${id}`);
+  return result.data;
+};
+
+const getOneName = async (name) => {
+  const result = await axios.get(`http://localhost:3002/persons?name=${name}`);
+  return result.data;
 };
 
 const deleteOne = async (id) => {
-  try {
-    const result = await axios.delete(`http://localhost:3002/persons/${id}`);
-    return result.data;
-  } catch (e) {
-    console.error(e);
-    if (e.status === 404) return {};
-    return null;
-  }
+  if (!id) throw new StatusError(400, "An id must be specified");
+
+  const result = await axios.delete(`http://localhost:3002/persons/${id}`);
+  return result.data;
 };
 
-export { getAll, getOne, deleteOne };
+const createOne = async (id, name, number) => {
+  if (!id) throw new StatusError(500, "Error asigning a unique id, try again");
+  if (!name)
+    throw new StatusError(400, "A name must be specified for the new person");
+  if (!number)
+    throw new StatusError(400, "A number must be specified for the new person");
+
+  const same_name = await getOneName(name);
+  console.log(same_name);
+  if (same_name && same_name.length > 0)
+    throw new StatusError(
+      400,
+      `${name} is already in use, choose a different name`
+    );
+
+  const result = await axios.post("http://localhost:3002/persons", {
+    id: id.toString(),
+    name,
+    number,
+  });
+  return result.data;
+};
+
+export { getAll, getOne, createOne, deleteOne };
