475c4b751455919d2ae341da0f97837a4e0e7ccb
PabloRius <pablogrius@gmail.com>
2025-01-02 19:23:19 +0000

    Add number validation (3.20)

--- part3/models/person.js
+++ part3/models/person.js
@@ -17,7 +17,17 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
-  number: { type: String, minLength: 6, required: true },
+  number: {
+    type: String,
+    required: true,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
