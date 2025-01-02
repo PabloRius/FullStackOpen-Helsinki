c55a78e16c3f3a41b48155e19b7892ffbed57a51
PabloRius <pablogrius@gmail.com>
2025-01-02 10:24:29 +0000

    3.12

---
+++ part3/mongo.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const connectionString = `mongodb+srv://pablogrius:${password}@fullstackopen.flmxp.mongodb.net/FullStackOpen?retryWrites=true&w=majority&appName=FullStackOpen`;
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    if (name && number) {
+      const personSchema = new mongoose.Schema({
+        name: String,
+        number: String,
+      });
+
+      const Person = mongoose.model("Person", personSchema, "phonebook");
+
+      const person = new Person({ name, number });
+
+      person
+        .save()
+        .then(() => {
+          console.log(`Added ${name} number ${number} to phonebook`);
+          mongoose.connection.close();
+        })
+        .catch((err) => {
+          console.error(err);
+          mongoose.connection.close();
+        });
+    } else {
+      const personSchema = new mongoose.Schema({
+        name: String,
+        number: String,
+      });
+
+      const Person = mongoose.model("Person", personSchema, "phonebook");
+
+      Person.find({})
+        .then((persons) => {
+          console.log("Phonebook:");
+          persons.forEach((person) => {
+            console.log(`${person.name} ${person.number}`);
+          });
+          mongoose.connection.close();
+        })
+        .catch((err) => {
+          console.error(err);
+          mongoose.connection.close();
+        });
+    }
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
