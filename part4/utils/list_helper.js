d5e16cb86ea63e154d12734170852e5d84bdcbae
PabloRius <pablogrius@gmail.com>
2025-01-03 18:05:33 +0000

    Favorite blog testing

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,4 +9,13 @@ const totalLikes = (blogs) => {
     : 0;
 };
 
-export { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  let topBlog = blogs[0];
+  blogs.forEach((blog) => {
+    if (blog.likes > topBlog.likes) topBlog = blog;
+  });
+  return topBlog;
+};
+
+export { dummy, totalLikes, favoriteBlog };
