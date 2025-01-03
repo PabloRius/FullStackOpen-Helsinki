5a5160b9bd81e70de8935f4720a40fbf77123d3a
PabloRius <pablogrius@gmail.com>
2025-01-03 17:56:08 +0000

    4.4 further testing

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1;
 };
 
-export { dummy };
+const totalLikes = (blogs) => {
+  return blogs.length > 0
+    ? blogs.reduce((acc, current) => acc + current.likes, 0)
+    : 0;
+};
+
+export { dummy, totalLikes };
